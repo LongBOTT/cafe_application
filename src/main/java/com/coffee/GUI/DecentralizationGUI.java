1a0f201e309aba842b54dad02071909607762fa3
Nguyễn Hoàng Long <93486974+LongBOTT@users.noreply.github.com>
2024-03-11 17:56:12 +0700

    Update DecentralizationGUI.java

--- src/main/java/com/coffee/GUI/DecentralizationGUI.java
+++ src/main/java/com/coffee/GUI/DecentralizationGUI.java
@@ -6,6 +6,183 @@ import com.coffee.GUI.components.Layout1;
 import java.util.List;
 
 public class DecentralizationGUI extends Layout1 {
+
+    private RoundedPanel containerSearch;
+    private JLabel iconSearch;
+    private JTextField jTextFieldSearch;
+    private JButton jButtonSearch;
+    private JComboBox<String> jComboBoxSearch;
+    private List<Function> functions;
+    private RoleBLL roleBLL = new RoleBLL();
+    private DataTable dataTable;
+    private RoundedScrollPane scrollPane;
+    private int indexColumnDetail = -1;
+    private int indexColumnEdit = -1;
+    private int indexColumnRemove = -1;
+    private boolean detail = false;
+    private boolean edit = false;
+    private boolean remove = false;
+    private String[] columnNames;
+
     public DecentralizationGUI(List<Function> functions) {
+        super();
+        this.functions = functions;
+        if (functions.stream().anyMatch(f -> f.getName().equals("view")))
+            detail = true;
+        if (functions.stream().anyMatch(f -> f.getName().equals("edit")))
+            edit = true;
+        if (functions.stream().anyMatch(f -> f.getName().equals("remove")))
+            remove = true;
+        init(functions);
     }
+
+    private void init(List<Function> functions) {
+        containerSearch = new RoundedPanel();
+        iconSearch = new JLabel();
+        jTextFieldSearch = new JTextField();
+        jButtonSearch = new JButton("Tìm kiếm");
+        jComboBoxSearch = new JComboBox<>(new String[]{"Chức vụ", "Chức năng", "Quyền hạn"});
+
+        columnNames = new String[]{"Chức vụ", "Chức năng", "Quyền hạn"};
+        if (detail) {
+            columnNames = Arrays.copyOf(columnNames, columnNames.length + 1);
+            indexColumnDetail = columnNames.length - 1;
+            columnNames[indexColumnDetail] = "Xem";
+        }
+
+        if (edit) {
+            columnNames = Arrays.copyOf(columnNames, columnNames.length + 1);
+            indexColumnEdit = columnNames.length - 1;
+            columnNames[indexColumnEdit] = "Sửa";
+        }
+
+        if (remove) {
+            columnNames = Arrays.copyOf(columnNames, columnNames.length + 1);
+            indexColumnRemove = columnNames.length - 1;
+            columnNames[indexColumnRemove] = "Xoá";
+        }
+
+        dataTable = new DataTable(new Object[0][0], columnNames,
+                e -> selectFunction(),
+                detail, edit, remove, 3); // table hiển thị các thuộc tính "Mã NCC", "Tên NCC", "SĐT", "Email" nên điền 4
+        scrollPane = new RoundedScrollPane(dataTable, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setPreferredSize(new Dimension(1165, 680));
+        bottom.add(scrollPane, BorderLayout.CENTER);
+
+        containerSearch.setLayout(new MigLayout("", "10[]10[]10", ""));
+        containerSearch.setBackground(new Color(245, 246, 250));
+        containerSearch.setPreferredSize(new Dimension(280, 40));
+        SearchPanel.add(containerSearch);
+
+        iconSearch.setIcon(new FlatSVGIcon("icon/search.svg"));
+        containerSearch.add(iconSearch);
+
+        jTextFieldSearch.setBackground(new Color(245, 246, 250));
+        jTextFieldSearch.setBorder(BorderFactory.createEmptyBorder());
+        jTextFieldSearch.putClientProperty("JTextField.placeholderText", "Nhập nội dung tìm kiếm");
+        jTextFieldSearch.setPreferredSize(new Dimension(250, 30));
+        jTextFieldSearch.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                searchDecentralizations();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                searchDecentralizations();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                searchDecentralizations();
+            }
+        });
+        containerSearch.add(jTextFieldSearch);
+
+        jButtonSearch.setBackground(new Color(29, 78, 216));
+        jButtonSearch.setForeground(Color.white);
+        jButtonSearch.setPreferredSize(new Dimension(100, 30));
+        jButtonSearch.addActionListener(e -> searchDecentralization());
+        SearchPanel.add(jButtonSearch);
+
+        jComboBoxSearch.setBackground(new Color(29, 78, 216));
+        jComboBoxSearch.setForeground(Color.white);
+        jComboBoxSearch.setPreferredSize(new Dimension(100, 30));
+        jComboBoxSearch.addActionListener(e -> selectSearchFilter());
+        SearchPanel.add(jComboBoxSearch);
+
+        //loadDataTable(supplierBLL.getData(supplierBLL.searchSuppliers("deleted = 0")));
+
+        RoundedPanel refreshPanel = new RoundedPanel();
+        refreshPanel.setLayout(new GridBagLayout());
+        refreshPanel.setPreferredSize(new Dimension(130, 40));
+        refreshPanel.setBackground(new Color(217, 217, 217));
+        refreshPanel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        refreshPanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                refresh();
+            }
+        });
+        FunctionPanel.add(refreshPanel);
+
+        JLabel refreshLabel = new JLabel("Làm mới");
+        refreshLabel.setFont(new Font("Public Sans", Font.PLAIN, 13));
+        refreshLabel.setIcon(new FlatSVGIcon("icon/refresh.svg"));
+        refreshPanel.add(refreshLabel);
+
+        if (functions.stream().anyMatch(f -> f.getName().equals("add"))) {
+            RoundedPanel roundedPanel = new RoundedPanel();
+            roundedPanel.setLayout(new GridBagLayout());
+            roundedPanel.setPreferredSize(new Dimension(130, 40));
+            roundedPanel.setBackground(new Color(217, 217, 217));
+            roundedPanel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+            roundedPanel.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mousePressed(MouseEvent e) {
+                    new AddSupplierGUI();
+                    refresh();
+                }
+            });
+            FunctionPanel.add(roundedPanel);
+
+            JLabel panel = new JLabel("Thêm mới");
+            panel.setFont(new Font("Public Sans", Font.PLAIN, 13));
+            panel.setIcon(new FlatSVGIcon("icon/add.svg"));
+            roundedPanel.add(panel);
+        }
+        if (functions.stream().anyMatch(f -> f.getName().equals("excel"))) {
+            RoundedPanel roundedPanel = new RoundedPanel();
+            roundedPanel.setLayout(new GridBagLayout());
+            roundedPanel.setPreferredSize(new Dimension(130, 40));
+            roundedPanel.setBackground(new Color(217, 217, 217));
+            roundedPanel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+            FunctionPanel.add(roundedPanel);
+
+            JLabel panel = new JLabel("Xuất Excel");
+            panel.setFont(new Font("Public Sans", Font.PLAIN, 13));
+            panel.setIcon(new FlatSVGIcon("icon/excel.svg"));
+            roundedPanel.add(panel);
+        }
+        if (functions.stream().anyMatch(f -> f.getName().equals("pdf"))) {
+            RoundedPanel roundedPanel = new RoundedPanel();
+            roundedPanel.setLayout(new GridBagLayout());
+            roundedPanel.setPreferredSize(new Dimension(130, 40));
+            roundedPanel.setBackground(new Color(217, 217, 217));
+            roundedPanel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+            FunctionPanel.add(roundedPanel);
+
+            JLabel panel = new JLabel("Xuất PDF");
+            panel.setFont(new Font("Public Sans", Font.PLAIN, 13));
+            panel.setIcon(new FlatSVGIcon("icon/pdf.svg"));
+            roundedPanel.add(panel);
+        }
+    }
+
+    //public void refresh() {
+        //jTextFieldSearch.setText("");
+        //jComboBoxSearch.setSelectedIndex(0);
+        //loadDataTable(supplierBLL.getData(supplierBLL.searchSuppliers("deleted = 0")));
+    //}
+
 }
