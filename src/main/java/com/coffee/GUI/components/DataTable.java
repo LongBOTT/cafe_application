514e6b9c0c81caae9305d72f64b446bb4cea8118
NguyenMinhThuan <95559097+NguyenMinhThuanK12@users.noreply.github.com>
2024-03-31 14:57:24 +0700

    .

--- src/main/java/com/coffee/GUI/components/DataTable.java
+++ src/main/java/com/coffee/GUI/components/DataTable.java
@@ -1,10 +1,10 @@
 package com.coffee.GUI.components;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
-import javax.swing.table.TableModel;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -238,6 +238,93 @@ public class DataTable extends JTable {
         JTableHeader jTableHeader = getTableHeader();
         jTableHeader.setBackground(new Color(217, 217, 217));
     }
+    public DataTable(Object[][] data, Object[] columnNames, ActionListener actionListener,
+                     boolean detail, boolean edit, boolean remove, int numberOfColumns,boolean material, int quantity) {
+        super(new DefaultTableModel(data, columnNames) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return column == 4;
+            }
+
+        });
+
+
+        getModel().addTableModelListener(new TableModelListener() {
+            @Override
+            public void tableChanged(TableModelEvent e) {
+                if (e.getType() == TableModelEvent.UPDATE) {
+                    int row = e.getFirstRow();
+                    int col = e.getColumn();
+                    if (col == 4) {
+                        Object valueAtColumn4 =  getModel().getValueAt(row, 4);
+                        Object valueAtColumn3 =  getModel().getValueAt(row, 3);
+                        if (valueAtColumn4 != null && valueAtColumn3 != null) {
+                            try {
+                                Double newValue = Double.parseDouble(valueAtColumn4.toString())* Double.parseDouble(valueAtColumn3.toString());
+                                getModel().setValueAt(newValue, row, 5);
+                            } catch (NumberFormatException ex) {
+                                JOptionPane.showMessageDialog(null, "Số lượng không phải là số hợp lệ.", "Lỗi", JOptionPane.ERROR_MESSAGE);
+                                SwingUtilities.invokeLater(() -> {
+                                    editCellAt(row, col);
+                                    getEditorComponent().requestFocusInWindow();
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        for (int i = 0; i < getColumnCount(); i++) {
+            setDefaultRenderer(getColumnClass(i), new CustomTableCellRenderer());
+        }
+        for (int i = numberOfColumns; i < getColumnCount(); i++) {
+            getColumnModel().getColumn(i).setCellRenderer(new CustomPanelRenderer());
+            getColumnModel().getColumn(i).setMaxWidth(50);
+        }
+
+        getTableHeader().setFont(new Font("Public Sans", Font.BOLD | Font.ITALIC, 15));
+        getTableHeader().setReorderingAllowed(false);
+        getTableHeader().setResizingAllowed(false);
+
+        setFont(new Font("Public Sans", Font.PLAIN, 15));
+        setAutoCreateRowSorter(false);
+        setRowHeight(40);
+        setSelectionBackground(new Color(220, 221, 225, 221));
+        setSelectionForeground(Color.BLACK);
+        setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                int row = rowAtPoint(e.getPoint());
+                int col = columnAtPoint(e.getPoint());
+
+                if ((col == numberOfColumns && row != -1) || (col == numberOfColumns + 1 && row != -1) || (col == numberOfColumns + 2 && row != -1) || (col == quantity && row != -1)) {
+                    if (actionListener != null) {
+                        actionListener.actionPerformed(null);
+                    }
+                }
+                if (!detail && !edit && remove && col != numberOfColumns && row != -1 && col != numberOfColumns + 1 && col != numberOfColumns + 2) {
+                    if (actionListener != null) {
+                        actionListener.actionPerformed(null);
+                    }
+                }
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                if (getSelectedRow() == -1) {
+                    lastSelectedRow = -1;
+                }
+            }
+        });
+
+
+        JTableHeader jTableHeader = getTableHeader();
+        jTableHeader.setBackground(new Color(217, 217, 217));
+    }
+
 
 }
 
