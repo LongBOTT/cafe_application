ccd6fb2ff8e23e537c82e9bde28679a1ef35635c
Nguyễn Hoàng Long <93486974+LongBOTT@users.noreply.github.com>
2024-04-28 12:48:17 +0700

    upd

--- src/main/java/com/coffee/main/Cafe_Application.java
+++ src/main/java/com/coffee/main/Cafe_Application.java
@@ -1,5 +1,11 @@
 package com.coffee.main;
 
+import com.coffee.BLL.ProductBLL;
+import com.coffee.BLL.ReceiptBLL;
+import com.coffee.BLL.Receipt_DetailBLL;
+import com.coffee.DTO.Product;
+import com.coffee.DTO.Receipt;
+import com.coffee.DTO.Receipt_Detail;
 import com.coffee.GUI.HomeGUI;
 import com.coffee.GUI.LoginGUI;
 
@@ -10,6 +16,11 @@ import com.formdev.flatlaf.fonts.roboto.FlatRobotoFont;
 
 import javax.swing.*;
 import java.awt.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Cafe_Application {
     public static LoginGUI loginGUI;
@@ -45,7 +56,70 @@ public class Cafe_Application {
 //        new AddPayrollGUI();
 //        new DetailPayroll_DetailGUI(new Payroll_DetailBLL().searchPayroll_Details().get(0), new PayrollBLL().searchPayrolls().get(0));
 
+///       tao du lieu random cho hoa don
 
+
+//        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//        LocalDateTime start = LocalDateTime.of(2023, 1, 1, 0, 0);
+//        LocalDateTime end = LocalDateTime.of(2024, 4, 28, 0, 0);
+//
+//        List<Product> productList = new ProductBLL().searchProducts("deleted = 0");
+//        int id = 1;
+//        while (!start.equals(end)) {
+//            for (int j = 0; j < 5; j++) {
+//                Random random = new Random();
+//
+//                // Tạo thời điểm ngẫu nhiên trong ngày 1/1/2023
+//                int gio = random.nextInt(24); // Giờ từ 0 đến 23
+//                int phut = random.nextInt(60); // Phút từ 0 đến 59
+//                int giay = random.nextInt(60); // Giây từ 0 đến 59
+//
+//                LocalDateTime invoice_date = start.withHour(gio).withMinute(phut).withSecond(giay);
+//
+//                List<Integer> chosenIndices = new ArrayList<>();
+//                List<Product> randomProducts = new ArrayList<>();
+//                for (int i = 0; i < 5; i++) {
+//                    int index;
+//                    do {
+//                        index = random.nextInt(productList.size());
+//                    } while (chosenIndices.contains(index));
+//                    chosenIndices.add(index);
+//                    randomProducts.add(productList.get(index));
+//                }
+//
+//                int staffId = random.nextInt(2) + 5;
+//                Receipt receipt = new Receipt(id, staffId, invoice_date, 0, 0, 0, 0, 0, 0);
+//
+//                double total = 0;
+//                List<Receipt_Detail> receipt_details = new ArrayList<>();
+//                for (Product product : randomProducts) {
+//                    Receipt_Detail receipt_detail = new Receipt_Detail();
+//                    receipt_detail.setReceipt_id(receipt.getId());
+//                    receipt_detail.setProduct_id(product.getId());
+//                    receipt_detail.setSize(product.getSize());
+//                    int quantity = random.nextInt(5) + 1;
+//                    receipt_detail.setQuantity(quantity);
+//                    receipt_detail.setPrice(quantity * product.getPrice());
+//                    receipt_detail.setNotice(" ");
+//
+//                    total += receipt_detail.getPrice();
+//                    receipt_details.add(receipt_detail);
+//                }
+//
+//                receipt.setTotal_price(total);
+//                receipt.setTotal(total);
+//                receipt.setReceived(total);
+//
+//                new ReceiptBLL().addReceipt(receipt);
+//
+//                for (Receipt_Detail receipt_detail : receipt_details) {
+//                    new Receipt_DetailBLL().addReceipt_Detail(receipt_detail);
+//                }
+//
+//                id += 1;
+//            }
+//            start = start.plusDays(1);
+//        }
     }
 
     public static void exit(int status) {
