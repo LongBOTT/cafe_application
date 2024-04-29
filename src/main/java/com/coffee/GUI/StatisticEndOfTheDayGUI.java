3df634fa36b1e44aa31b0d640d9a8c5e39690e9b
NguyenMinhThuan <95559097+NguyenMinhThuanK12@users.noreply.github.com>
2024-04-30 00:34:47 +0700

    .

--- src/main/java/com/coffee/GUI/StatisticEndOfTheDayGUI.java
+++ src/main/java/com/coffee/GUI/StatisticEndOfTheDayGUI.java
@@ -2,6 +2,7 @@ package com.coffee.GUI;
 
 import com.coffee.GUI.components.DatePicker;
 import com.coffee.GUI.components.RoundedPanel;
+import com.formdev.flatlaf.extras.FlatSVGIcon;
 import net.miginfocom.swing.MigLayout;
 import raven.datetime.component.date.DateEvent;
 import raven.datetime.component.date.DateSelectionListener;
@@ -11,6 +12,8 @@ import java.awt.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StatisticEndOfTheDayGUI extends JPanel {
     private JScrollPane scrollPane;
@@ -22,7 +25,10 @@ public class StatisticEndOfTheDayGUI extends JPanel {
     private JLabel titleLabel;
 
     private JPanel centerPanel;
-
+    private static final int PANEL_WIDTH = 885;
+    private static final int PANEL_HEIGHT = 40;
+    private static final Dimension LABEL_SIZE = new Dimension(150, 30);
+    Map<JPanel, Boolean> expandedStateMap = new HashMap<>(); // Biến để theo dõi trạng thái của nút btnDetail
     public StatisticEndOfTheDayGUI() {
         setBackground(Color.WHITE);
         setPreferredSize(new Dimension(1000, 700));
@@ -38,7 +44,7 @@ public class StatisticEndOfTheDayGUI extends JPanel {
         initRightBartEndOfDayPanel();
 
         content = new JPanel();
-        content.setLayout(new FlowLayout(FlowLayout.CENTER,0,10));
+        content.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 10));
         content.setPreferredSize(new Dimension(730, 700));
         content.setBackground(new Color(255, 255, 255));
         add(content, BorderLayout.CENTER);
@@ -87,60 +93,194 @@ public class StatisticEndOfTheDayGUI extends JPanel {
         titletTopPanel.add(dateReportPanel, BorderLayout.SOUTH);
 
     }
-
     private void initCenterContent() {
         centerPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
-        centerPanel.setPreferredSize(new Dimension(885, 580));
+        centerPanel.setPreferredSize(new Dimension(PANEL_WIDTH, 580));
         content.add(centerPanel);
         centerPanel.setBackground(Color.WHITE);
 
-        setUpLabelSalesReports();
-
+        setUpSalesReports();
     }
 
-    private void setUpLabelSalesReports() {
+    private void setUpSalesReports() {
         centerPanel.removeAll();
 
-        JPanel salesLabelPanel = new JPanel(); // panel chứa các nhãn
-        salesLabelPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 100, 10));
-        salesLabelPanel.setPreferredSize(new Dimension(885, 40));
-        salesLabelPanel.setBackground(new Color(178, 232, 255));
-        salesLabelPanel.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, new Color(202, 202, 202))); // tạo viền cho panel
-
-        String[] labels = {"Mã hóa đơn", "Nhân viên tạo", "Thời gian", "SLSP", "Doanh thu"};
-
-        for (String label : labels) {
-            JLabel jLabel = new JLabel(label);
-            jLabel.setFont(new Font("Inter", Font.BOLD, 13));
-            salesLabelPanel.add(jLabel);
-        }
-
+        JPanel salesLabelPanel = createLabelPanel(new Color(178, 232, 255), new Color(202, 202, 202));
+        addLabelsToPanel(salesLabelPanel, new String[]{"Mã hóa đơn", "Thời gian", "Nhân viên tạo", "SLSP", "Doanh thu"});
         centerPanel.add(salesLabelPanel);
 
-        //
-        JPanel salesLabelPanel1 = new JPanel(); // panel chứa kết quả
-        salesLabelPanel1.setLayout(new MigLayout("", "90[]580[]90", "10[]10"));
-        salesLabelPanel1.setPreferredSize(new Dimension(885, 40));
-        salesLabelPanel1.setBackground(new Color(242, 238, 214));
-        salesLabelPanel1.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, new Color(202, 202, 202))); // tạo viền cho panel
+        JPanel salesDataPanel = createLabelPanel(new Color(242, 238, 214), new Color(202, 202, 202));
+        addLabelsToPanel(salesDataPanel, new String[]{"Hóa đơn: 10", "", "", "10", "10,000"});
+        centerPanel.add(salesDataPanel);
 
-        JLabel quantityBill = new JLabel();
-        quantityBill.setText("Hóa đơn: 10");
-        quantityBill.setFont(new Font("Inter", Font.PLAIN, 13));
-        salesLabelPanel1.add(quantityBill);
+        String[][] reportsData = {
+                {"12-12-2021", "12:00", "10", "20,000"},
+                {"13-12-2021", "13:00", "20", "20,000"},
+                {"14-12-2021", "14:00", "30", "20,000"},
+                {"15-12-2021", "15:00", "40", "20,000"},
+                {"16-12-2021", "16:00", "50", "20,000"},
+                // Thêm các dữ liệu khác nếu cần
+        };
 
-        JLabel revenue = new JLabel();
-        revenue.setText("1000000");
-        revenue.setFont(new Font("Inter", Font.PLAIN, 13));
-        salesLabelPanel1.add(revenue);
-
-        centerPanel.add(salesLabelPanel1);
-
+        // Tạo panel báo cáo cho mỗi dòng trong mảng dữ liệu
+        for (String[] data : reportsData) {
+            JPanel reportPanel = createPanelReport(data[0], data[1], data[2], data[3]);
+            expandedStateMap.put(reportPanel, false); // Ban đầu, tất cả các panel đều không mở rộng
+            centerPanel.add(reportPanel);
+        }
 
         centerPanel.repaint();
         centerPanel.revalidate();
+
     }
 
+    private JPanel createLabelPanel(Color background, Color border) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new MigLayout("", "10[]30[]30[]30[]30[]10", ""));
+        panel.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
+        panel.setBackground(background);
+        panel.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, border));
+        return panel;
+    }
+
+    private void addLabelsToPanel(JPanel panel, String[] labels) {
+        int i = 0;
+        for (String label : labels) {
+            JLabel jLabel = new JLabel(label);
+            jLabel.setFont(new Font("Inter", Font.BOLD, 13));
+            jLabel.setPreferredSize(LABEL_SIZE);
+            jLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            if (i==0)
+                jLabel.setHorizontalAlignment(SwingConstants.LEFT);
+            if (i==4)
+                jLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+            panel.add(jLabel);
+            i++;
+        }
+    }
+
+    private JPanel createPanelReport(String date, String hour, String quantity, String revenue) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new MigLayout("", "10[]30[]30[]30[]30[]10", ""));
+        panel.setPreferredSize(new Dimension(PANEL_WIDTH, 50));
+        panel.setBackground(Color.WHITE);
+        panel.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, new Color(202, 202, 202)));
+
+        JPanel containerBtn = new JPanel();
+        containerBtn.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+        containerBtn.setPreferredSize(new Dimension(150, 30));
+        containerBtn.setBackground(Color.WHITE);
+
+        JButton btnDetail = new JButton();
+        btnDetail.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        btnDetail.setIcon(new FlatSVGIcon("icon/add-square-svgrepo-com.svg")); // Đặt biểu tượng ban đầu
+        btnDetail.setBorderPainted(false);
+        btnDetail.setFocusPainted(false);
+        btnDetail.setContentAreaFilled(false);
+        btnDetail.addActionListener(e -> {
+            JPanel selectedPanel = (JPanel) btnDetail.getParent().getParent(); // Lấy panel cha của nút
+            togglePanel(btnDetail, selectedPanel);// Kích hoạt chức năng mở rộng/ thu gọn khi nhấn nút
+        });
+
+        JPanel panel1 = new JPanel();
+        panel1.setBackground(Color.WHITE);
+        panel1.setLayout(new BorderLayout());
+
+        JLabel lblDate = new JLabel(date);
+        lblDate.setFont(new Font("Inter", Font.PLAIN, 13));
+        lblDate.setForeground(new Color(33, 160, 223));
+
+        JLabel lblHour = new JLabel(hour);
+        lblHour.setFont(new Font("Inter", Font.PLAIN, 13));
+        lblHour.setForeground(new Color(33, 160, 223));
+
+        panel1.add(lblHour, BorderLayout.SOUTH);
+        panel1.add(lblDate, BorderLayout.NORTH);
+
+        containerBtn.add(btnDetail);
+        containerBtn.add(panel1);
+
+        JLabel lbl = new JLabel();
+        lbl.setPreferredSize(new Dimension(150, 50));
+
+        JLabel lbl1 = new JLabel();
+        lbl1.setPreferredSize(new Dimension(150, 50));
+
+        JLabel lblQuantity = new JLabel(quantity);
+        lblQuantity.setFont(new Font("Inter", Font.PLAIN, 13));
+        lblQuantity.setHorizontalAlignment(SwingConstants.CENTER);
+        lblQuantity.setPreferredSize(new Dimension(150, 50));
+
+        JLabel lblRevenue = new JLabel(revenue);
+        lblRevenue.setFont(new Font("Inter", Font.PLAIN, 13));
+        lblRevenue.setPreferredSize(new Dimension(150, 50));
+        lblRevenue.setHorizontalAlignment(SwingConstants.RIGHT);
+
+        panel.add(containerBtn);
+        panel.add(lbl);
+        panel.add(lbl1);
+        panel.add(lblQuantity);
+        panel.add(lblRevenue);
+
+       return panel;
+    }
+
+
+    private void togglePanel(JButton btnDetail, JPanel selectedPanel) {
+        boolean isExpanded = expandedStateMap.get(selectedPanel); // Lấy trạng thái mở rộng của panel được chọn
+        if (isExpanded) {
+            removeDetailPanels(selectedPanel);
+            btnDetail.setIcon(new FlatSVGIcon("icon/add-square-svgrepo-com.svg"));
+        } else {
+            addDetailPanel(selectedPanel);
+            btnDetail.setIcon(new FlatSVGIcon("icon/minus-square-svgrepo-com.svg"));
+        }
+        expandedStateMap.put(selectedPanel, !isExpanded); // Cập nhật trạng thái mở rộng
+    }
+
+    private void removeDetailPanels(JPanel selectedPanel) {
+        int selectedIndex = centerPanel.getComponentZOrder(selectedPanel);
+        int nextPanelIndex = centerPanel.getComponentCount();
+
+        // Tìm vị trí của panel cha tiếp theo trong centerPanel
+        for (int i = selectedIndex + 1; i < centerPanel.getComponentCount(); i++) {
+            Component component = centerPanel.getComponent(i);
+           if (((JPanel) component).getClientProperty("isDetailPanel") == null) {
+                nextPanelIndex = i;
+                break;
+            }
+        }
+
+        Component[] components = centerPanel.getComponents();
+        for (int i = selectedIndex + 1; i < nextPanelIndex; i++) {
+            Component component = centerPanel.getComponent(i);
+            if (component instanceof JPanel && ((JPanel) component).getClientProperty("isDetailPanel") != null) {
+                centerPanel.remove(component);
+            } else {
+                return; // Kết thúc vòng lặp nếu gặp panel không phải là chi tiết
+            }
+        }
+
+        centerPanel.revalidate();
+        centerPanel.repaint();
+    }
+    private void addDetailPanel(JPanel selectedPanel) {
+        String[] labels = {"HD000001", "12:00", "Nguyen Vam A", "10", "10,000"};
+        JPanel panel = createPanelDetail(labels);
+
+        // Thêm panel chi tiết vào dưới panel được chọn
+        int selectedIndex = centerPanel.getComponentZOrder(selectedPanel);
+        centerPanel.add(panel, selectedIndex + 1);
+        centerPanel.revalidate();
+        centerPanel.repaint();
+    }
+
+    private JPanel createPanelDetail(String[] labels) {
+        JPanel panel = createLabelPanel(Color.WHITE, new Color(202, 202, 202));
+        panel.putClientProperty("isDetailPanel", true); // Đặt thuộc tính cho panel chi tiết
+        addLabelsToPanel(panel, labels);
+        return panel;
+    }
 
     private void initRightBartEndOfDayPanel() {
         JPanel jPanel = new JPanel(new MigLayout("", "5[]5", "10[]10"));
